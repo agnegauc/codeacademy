7c8601231d7eaaf9b0a84d6bd398ddec9549bc16
agnegauc <agne.gauciute@gmail.com>
2022-09-24 16:10:31 +0300

    09.24

--- JavaScript/Practice 38 (form-handling tasks 6)/forms.js
+++ JavaScript/Practice 38 (form-handling tasks 6)/forms.js
@@ -1,3 +1,28 @@
-// Ex. 2
+// My version:
 
-const year = document.querySelector("input");
+// const year = document.querySelector("input");
+
+// document.querySelector("form").addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   if (year.value % 100 === 0 && year % 400 === 0) {
+//     document.querySelector("p").textContent = "Leap year";
+//   } else if (year.value % 4 === 0) {
+//     document.querySelector("p").textContent = "Leap year";
+//   } else document.querySelector("p").textContent = "Not leap";
+// });
+
+// codeAcademy version:
+
+function isLeapYear(year) {
+  return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0; // Sąlyga to be leap year
+}
+
+document.querySelector("form").addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const year = document.querySelector("input").value;
+  if (isLeapYear(year)) {
+    document.querySelector("p").textContent = "Leap year";
+  } else document.querySelector("p").textContent = "Not leap";
+}
