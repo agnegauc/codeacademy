b010a8395fe4437e2c8f0f71cdf65934f0a5340c
agnegauc <agne.gauciute@gmail.com>
2022-10-26 21:43:29 +0300

    10.26

--- JavaScript/Practice 83 (fetch POST) CAO-24/script.js
+++ JavaScript/Practice 83 (fetch POST) CAO-24/script.js
@@ -1,20 +1,5 @@
 const state = {};
 
-const fetchProperties = async () => {
-  try {
-    const response = await fetch("https://radial-reinvented-shoe.glitch.me");
-    state.properties = await response.json();
-    renderPropertyCards(state.properties);
-    renderFilterButtons([
-      ...new Set(state.properties.map((entry) => entry.city)),
-    ]);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-fetchProperties();
-
 const renderPropertyCards = (properties) => {
   const propertyContainer = document.querySelector("#properties-container");
   propertyContainer.textContent = ""; // Select the whole block (in other practices - body) and make sure it's empty before forEach
@@ -38,3 +23,54 @@ const renderPropertyCards = (properties) => {
     propertyContainer.append(cardContainer);
   });
 };
+
+// Map paima iš duomenų visus miestus; new Set išmeta pasikartojančius:
+// console.log([...new Set(state.properties.map((entry) => entry.city))]);
+
+const renderFilterButtons = (cities) => {
+  // į parametrus (as cities) paduosim an array of cities that we will get later (logic in the commented console.log above)
+  cities.forEach((city) => {
+    const filterButton = document.createElement("button");
+    const filtersContainer = document.querySelector("#filters-container");
+    filterButton.textContent = city;
+    filterButton.setAttribute("id", `${city}-container`);
+    filtersContainer.append(filterButton);
+  });
+};
+
+const getProperties = async () => {
+  try {
+    const response = await fetch("https://radial-reinvented-shoe.glitch.me");
+    const fetchedProperties = await response.json();
+    return fetchedProperties; // reikia returnint būtinai!
+    renderFilterButtons([
+      ...new Set(state.properties.map((entry) => entry.city)),
+    ]);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const properties = await getProperties(); // kviečiam būtinai su await
+state.properties = properties;
+
+renderPropertyCards(state.properties);
+renderFilterButtons([...new Set(state.properties.map((entry) => entry.city))]);
+
+document
+  .querySelector("#filters-container")
+  .addEventListener("click", (event) => {
+    const currentlyDisplayedPropertiesCount = document.querySelector(
+      "#properties-container"
+    ).childElementCount;
+    const totalPropertiesCount = state.properties.length;
+    const city = event.target.innerText;
+
+    if (currentlyDisplayedPropertiesCount === totalPropertiesCount) {
+      renderPropertyCards(
+        state.properties.filter((property) => property.city === city)
+      );
+    } else {
+      renderPropertyCards(state.properties);
+    }
+  });
